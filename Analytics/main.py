701cb7a029bbf13c641ccb924efc2ae62d97eb3c
kluhini96 <kieranhalliday96@gmail.com>
2018-03-04 10:39:03 -0800

    Updating python script

--- Analytics/main.py
+++ Analytics/main.py
@@ -8,10 +8,10 @@ totalDist = 0;
 
 for x in range (0, len(data["Wells"])):
     try:
-        pprint(data["Wells"][x]["depth"]);
+        pprint(int(data["Wells"][x]["depth"]));
         count+=1;
-        totalDist += data["Wells"][x]["depth"];
+        totalDist += int(data["Wells"][x]["depth"]);
     except:
         pprint("No element depth");
-
+        
 pprint("Average Depth: " + str(float(totalDist)/float(count)));
